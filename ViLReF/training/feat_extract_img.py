cd3ec4877043ef6562ddbca27c853f74aa28d56e
Nisreen Hisham <96303049+NancyHisham@users.noreply.github.com>
2025-05-02 01:54:40 +0300

    Update feat_extract_img.py

--- ViLReF/training/feat_extract_img.py
+++ ViLReF/training/feat_extract_img.py
@@ -4,6 +4,8 @@ class feat_extract_img():
         self.model = model
 
     def __call__(self, imgs):
-        return self.model.module.encode_image_featExt(imgs)
-
-        
+        model = self.model
+        if hasattr(model, "module"):
+            return model.module.encode_image_featExt(imgs)
+        else:
+            return model.encode_image_featExt(imgs)
